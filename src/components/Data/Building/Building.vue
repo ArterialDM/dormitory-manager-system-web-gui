33ab81963df3a6e8fb89c0a0997c092b72fecf53
ArterialDM <47880750+ArterialDM@users.noreply.github.com>
2020-02-14 16:15:51 +0800

    Create Building.vue

---
+++ src/components/Data/Building/Building.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-table
+            :data="tableData"
+            style="width: 100%">
+        <el-table-column
+                prop="buildingID"
+                label="宿舍楼">
+        </el-table-column>
+        <el-table-column
+                prop="student_live_count"
+                label="可入住学生量">
+        </el-table-column>
+        <el-table-column
+                prop="student_lived_count"
+                label="已入住学生量">
+        </el-table-column>
+        <el-table-column
+                prop="dormitory_have_count"
+                label="可用宿舍量">
+        </el-table-column>
+        <el-table-column
+                prop="dormitory_had_count"
+                label="已用宿舍量">
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    import axios from 'axios';
+    
+    export default {
+        name: "Building",
+        props: {
+            root: Boolean
+        },
+        data:function() {
+            return {
+                tableData: ''
+            }
+        },
+        created: function () {
+            const url = '/building';
+            axios.get(url).then(
+                response => {
+                    this.info=response;
+                    this.tableData=this.info.data;
+                }
+            )
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
