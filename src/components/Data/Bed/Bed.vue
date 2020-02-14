355bf7e30eba806b9ccb192e15576b894244c5cb
ArterialDM <47880750+ArterialDM@users.noreply.github.com>
2020-02-14 16:14:08 +0800

    Create Bed.vue

---
+++ src/components/Data/Bed/Bed.vue
@@ -0,0 +1,57 @@
+<template>
+    <el-table
+            :data="tableData"
+            style="width: 100%">
+        <el-table-column
+                label="床号"
+                prop="bedID">
+        </el-table-column>
+        <el-table-column
+                label="所在宿舍楼"
+                prop="buildingID">
+        </el-table-column>
+
+        <el-table-column
+                label="是否已用"
+                prop="can">
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: "Bed",
+        props: {
+            root: Boolean
+        },
+        data() {
+            return {
+                tableData: ''
+            }
+        },
+        methods: {
+            handleEdit(index, row) {
+                // eslint-disable-next-line no-console
+                console.log(index, row);
+                },
+            handleDelete(index, row) {
+                // eslint-disable-next-line no-console
+                console.log(index, row);
+            }
+        },
+        created: function () {
+            const url = '/bed';
+            axios.get(url).then(
+                response => {
+                    this.info=response;
+                    this.tableData=this.info.data;
+                }
+            )
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
